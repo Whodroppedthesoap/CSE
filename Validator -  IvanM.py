cac9ec7701e3a128c47cceb33a0b33f8c0165967
Whodroppedthesoap <42383861+Whodroppedthesoap@users.noreply.github.com>
2019-05-01 09:40:47 -0700

    yeahhhhhh

--- Validator -  IvanM.py
+++ Validator -  IvanM.py
@@ -5,19 +5,38 @@ fake = []
 
 def validate(num: str):
     if len(num) == 16:
-        last_num = int(num[15])
-        num -= last_num
-
-
-def reverse(num: str):
-    print(num[:: - 1])
+        num = list(num)
+        num.pop(15)
+        return True
+    else:
+        fake.append(num)
+    return False
 
 
 def last_num(num: str):
     last_num = int(num[15])
-    if last_num == :
+    if last_num % 2 == 1:
         return True
     return False
+
+
+def num_0dd(num: str):
+    num = int(num[0])
+    if num % 2 == 1:
+        return True
+    return False
+
+
+def multiply(num: str):
+    if num[0: 15] == [1, 3, 5, 7, 9]:
+        int(str(num_0dd) * 2)
+        if 
+        return True
+    return False
+
+
+def reverse(num: str):
+    print(num[:: - 1])
 
 
 with open("Book1.csv", 'r') as old_csv:
